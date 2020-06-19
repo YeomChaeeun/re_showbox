a6e3f9f2d560d3aba2c68254a8f6c4e6c534bdcd
YeomChaeEun <yce0222@gmail.com>
2020-06-19 17:09:07 +0900

    viewBox slide 완성, img file 첨부

--- coding/js/src/main_filmBox.js
+++ coding/js/src/main_filmBox.js
@@ -3,17 +3,17 @@
 (function($){
   // start
   var filmList = [
-    {no: '1', img:'movie_01.png', Pcimg:'movie_01.jpg', link: 'movie_01_info.html', imgNarr : '영화 01',title: 'movie_01', date: '2020.02.22', content: 'lorem ...', },
-    {no: '2', img:'movie_02.png', Pcimg:'movie_02.jpg', link: 'movie_02_info.html', imgNarr : '영화 02',title: 'movie_02', date: '2020.02.22', content: 'lorem ...', },
-    {no: '3', img:'movie_03.png', Pcimg:'movie_03.jpg', link: 'movie_03_info.html', imgNarr : '영화 03',title: 'movie_03', date: '2020.02.22', content: 'lorem ...', },
-    {no: '4', img:'movie_04.png', Pcimg:'movie_04.jpg', link: 'movie_04_info.html', imgNarr : '영화 04',title: 'movie_04', date: '2020.02.22', content: 'lorem ...', },
-    {no: '5', img:'movie_05.png', Pcimg:'movie_05.jpg', link: 'movie_05_info.html', imgNarr : '영화 05',title: 'movie_05', date: '2020.02.22', content: 'lorem ...', },
-    {no: '6', img:'movie_06.png', Pcimg:'movie_06.jpg', link: 'movie_06_info.html', imgNarr : '영화 06',title: 'movie_06', date: '2020.02.22', content: 'lorem ...', },
-    {no: '7', img:'movie_07.png', Pcimg:'movie_07.jpg', link: 'movie_07_info.html', imgNarr : '영화 07',title: 'movie_07', date: '2020.02.22', content: 'lorem ...', },
-    {no: '8', img:'movie_08.png', Pcimg:'movie_08.jpg', link: 'movie_08_info.html', imgNarr : '영화 08',title: 'movie_08', date: '2020.02.22', content: 'lorem ...', }
+    {no: '1', img:'movie_01.png', Pcimg:'movie_01.jpg', link: 'movie_01_info.html', imgNarr : '영화 01', title: '국제수사',  date: '2020', content: 'lorem ...', },
+    {no: '2', img:'movie_02.png', Pcimg:'movie_02.jpg', link: 'movie_02_info.html', imgNarr : '영화 02', title: '남산의 부장들', date: '2020.01.22', content: 'lorem ...', },
+    {no: '3', img:'movie_03.png', Pcimg:'movie_03.jpg', link: 'movie_03_info.html', imgNarr : '영화 03', title: '퍼펙트 맨', date: '2019.10.02', content: 'lorem ...', },
+    {no: '4', img:'movie_04.png', Pcimg:'movie_04.jpg', link: 'movie_04_info.html', imgNarr : '영화 04', title: '봉오동 전투', date: '2019.08.07', content: 'lorem ...', },
+    {no: '5', img:'movie_05.png', Pcimg:'movie_05.jpg', link: 'movie_05_info.html', imgNarr : '영화 05', title: '마담 싸이코', date: '2019.06', content: 'lorem ...', },
+    {no: '6', img:'movie_06.png', Pcimg:'movie_06.jpg', link: 'movie_06_info.html', imgNarr : '영화 06', title: '미성년',  date: '2019.04.11', content: 'lorem ...', },
+    {no: '7', img:'movie_07.png', Pcimg:'movie_07.jpg', link: 'movie_07_info.html', imgNarr : '영화 07', title: '돈',  date: '2019.03.20', content: 'lorem ...', },
+    {no: '8', img:'movie_08.png', Pcimg:'movie_08.jpg', link: 'movie_08_info.html', imgNarr : '영화 08', title: '뺑반',  date: '2019.01.30', content: 'lorem ...', }
   ];
 
-  var imgUrl ='../../img/film_poster/';
+  var imgUrl ='../img/movie_poster/';
 
   var filmBox = $('#filmBox');
   var filmArea = filmBox.find('.film_area');
@@ -25,7 +25,7 @@
             <a href="#">\
               <div class="img_bg"><span class="hidden">'+ filmList[i].imgNarr +'</span></div>\
               <dl><dt>'+ filmList[i].title +'</dt>\
-                <dd>'+ filmList[i].content +'</dd>\
+                <dd>'+ filmList[i].date +'</dd>\
               </dl>\
             </a>\
             </li>';
@@ -36,11 +36,11 @@
   for(; i<filmList.length; i++){	
     filmAreaUl.append( textEl(i) );
     filmAreaUl.children('li').eq(i).find('.img_bg').css({
-                                backgroundImage:'url('+ imgUrl +filmList[i].img +')',
-                                backgroundRepeat:'no-repeat',
-                                backgroundPosition:'50% 50%',
-                                backgroundSize:'cover'
-                              })
+      backgroundImage:'url('+ imgUrl + filmList[i].Pcimg +')',
+      backgroundRepeat:'no-repeat',
+      backgroundPosition:'50% 50%',
+      backgroundSize:'cover'
+    })
   }
 
   // end
--- coding/js/src/main_viewBox.js
+++ coding/js/src/main_viewBox.js
@@ -25,103 +25,130 @@
   var btnOk = true;
   var k;
   
-  var Url = '../../html/movie/';
+  var Url = '../img/viewBox/';
   var MainfilmList = [
-    {no: '1', img:'movie_01.png', Pcimg:'movie_01.jpg', title: 'movie_01', content: 'lorem ...', link: 'movie_01_info.html'},
-    {no: '2', img:'movie_02.png', Pcimg:'movie_02.jpg', title: 'movie_02', content: 'lorem ...', link: 'movie_02_info.html'},
-    {no: '3', img:'movie_03.png', Pcimg:'movie_03.jpg', title: 'movie_03', content: 'lorem ...', link: 'movie_03_info.html'},
-    {no: '4', img:'movie_04.png', Pcimg:'movie_04.jpg', title: 'movie_04', content: 'lorem ...', link: 'movie_04_info.html'},
-    {no: '5', img:'movie_05.png', Pcimg:'movie_05.jpg', title: 'movie_05', content: 'lorem ...', link: 'movie_05_info.html'},
-    {no: '6', img:'movie_06.png', Pcimg:'movie_06.jpg', title: 'movie_06', content: 'lorem ...', link: 'movie_06_info.html'},
-    {no: '7', img:'movie_07.png', Pcimg:'movie_07.jpg', title: 'movie_07', content: 'lorem ...', link: 'movie_07_info.html'},
-    {no: '8', img:'movie_08.png', Pcimg:'movie_08.jpg', title: 'movie_08', content: 'lorem ...', link: 'movie_08_info.html'}
+    {no: '1', img:'main_01.png', Pcimg:'main_01.jpg', title: '국제수사', content: '대한민국 촌구석 형사, 글로벌 범죄에 휘말렸다!<br/>“나 코리안 폴리스여!”<br/>동네급 형사의 국제급 수사가 시작된다!', link: 'movie_01_info.html'},
+    {no: '2', img:'main_02.png', Pcimg:'main_02.jpg', title: '남산의 부장들', content: 'lorem ...', link: 'movie_02_info.html'},
+    {no: '3', img:'main_03.png', Pcimg:'main_03.jpg', title: '퍼펙트 맨', content: 'lorem ...', link: 'movie_03_info.html'},
+    {no: '4', img:'main_04.png', Pcimg:'main_04.jpg', title: '봉오동 전투', content: 'lorem ...', link: 'movie_04_info.html'},
+    {no: '5', img:'main_05.png', Pcimg:'main_05.jpg', title: '마담 싸이코', content: 'lorem ...', link: 'movie_05_info.html'},
+    {no: '6', img:'main_06.png', Pcimg:'main_06.jpg', title: '미성년', content: 'lorem ...', link: 'movie_06_info.html'},
+    {no: '7', img:'main_07.png', Pcimg:'main_07.jpg', title: '돈', content: 'lorem ...', link: 'movie_07_info.html'},
+    {no: '8', img:'main_08.png', Pcimg:'main_08.jpg', title: '뺑반', content: 'lorem ...', link: 'movie_08_info.html'}
   ];
+
+
+
+  // 배열 이미지 담기
+  var i=0;
+  for(; i<MainfilmList.length; i++){	
+
+    indiTitleLi.eq(i).find('a').text(MainfilmList[i].title);
+    viewBannerBgLi.eq(i).html(MainfilmList[i].content);
+    viewFilmLi.eq(i).css({
+      backgroundImage:'url('+ Url + MainfilmList[i].Pcimg +')',
+      backgroundRepeat:'no-repeat',
+      backgroundPosition:'50% 0',
+      backgroundSize:'cover'
+    })
+  }
 
 
   
   // indicator title
-  var mvLength = 2; 
+  var mvLength = 3; 
   var indiLast = indiTitleLi.eq(-1*(mvLength+1)).nextAll('li');
   indiTitle.prepend(indiLast);
   indiTitleLi = indiTitle.find('li');
   var indiTitleLiLen = indiTitleLi.length;
-  indiTitle.css({width:300+'%'});
+  indiTitle.css({width:280+'%'});
   // console.log(indiTitleLiLen);
 
   var inditFirstSize = indiTitleLi.eq(1).outerWidth(true)*(mvLength-1);
   indiTitleLi.css({width:100/indiTitleLiLen+'%'});
-  indiTitle.css({marginLeft:-inditFirstSize+'px'});
+  indiTitle.css({marginLeft:-inditFirstSize-(inditFirstSize/26)+'px'});
   indiTitle.css({position:'relative'});
 
   indiTitleLi.find('a').on('click',function(e){
     e.preventDefault();
     if(btnOk){
-      btnOk = false;
       var clickIt = $(this).parent('li');
       var itIndex = clickIt.index();
+      indiN = itIndex;
       k = n;
-      n = itIndex-mvLength;
-      
-      ViewBannerTextFn(n,k);
-      viewFilmWrap.stop().animate({'marginLeft':n * -100 + '%'}, function(){
-        indiTitle.stop().animate({marginLeft: n*-inditFirstSize + '%'}, function(){
+      // console.log(vn,vk);
+      if(indiN<3){
+        // 이전 인디케이터 
+        btnOk = false;
+        n-=1;
+        ViewBannerTextFn(n,k);
 
+        viewFilmWrap.stop().animate({left: 100 + '%'}, function(){
+          if(n<0){
+            n=viewFilmLiLen-1;
+          }
+          viewFilmLi.eq(-1*(mvLength-1)).nextAll('li').prependTo(viewFilmWrap);
+          viewFilmWrap.css({left:0});
+          viewFilmLi = viewFilmWrap.find('li');
+          
+          indiTitle.animate({left:inditFirstSize/2+'px'},function(){
+            indiTitleLi.eq(-1*(mvLength-1)).nextAll('li').prependTo(indiTitle);
+            indiTitle.css({left:0});
+            indiTitleLi = indiTitle.find('li');
+          });  
+          indiTitleLi.eq(mvLength-1).addClass('active');
+          btnOk = true;
         });
-        btnOk = true;
-      });
-      indiTitleLi.eq(mvLength).prevAll('li').appendTo(indiTitle);
-      indiTitle.css({left:0});
-      indiTitleLi = indiTitle.find('li');
-      thisOk = true;
-      // indiTitle.stop().animate({marginLeft: n*-inditFirstSize + '%'}, function(){
-        
-      //   if(n>=indiTitleLi.length-2){
-      //     n = -1;
-      //     indiTitle.css({marginLeft: n*-inditFirstSize + '%'});
-      //   }
-      //   btnOk = true;
-      // });
+      }else if(indiN==3){
+        btnOk = false;
+        viewFilmWrap.stop().animate({left: 0}, function(){
+          btnOk = true;
+        });
 
-    }
-    // indiTitleLi.eq(n).siblings('li').removeClass('active');
-    // indiTitleLi.eq(n).addClass('active');
-  });
+      }else if(indiN>3){
+        // 이후 인디케이터 
+        btnOk = false;
+        n+=1;
+        ViewBannerTextFn(n,k);
 
-  var thisOk = true;
-  indiTitleLi.find('a').on('click',function(e){
-    e.preventDefault();
-    var thisIt = $(this)[0];
-    var nextBtn = indicator.children('.next')[0];
-
-    if(thisIt == nextBtn && thisOk){
-      thisOk = false;
-      indiTitle.animate({left:-inditFirstSize+'px'},function(){
-        indiTitleLi.eq(mvLength).prevAll('li').appendTo(indiTitle);
-        indiTitle.css({left:0});
-        indiTitleLi = indiTitle.find('li');
-        thisOk = true;
-      });
-    }else if(thisOk){
-      thisOk = false;
-      indiTitle.animate({left:inditFirstSize+'px'},function(){
-        indiTitleLi.eq(-1*(mvLength+1)).nextAll('li').prependTo(indiTitle);
-        indiTitle.css({left:0});
-        indiTitleLi = indiTitle.find('li');
-        thisOk = true;
-      });      
+        viewFilmWrap.stop().animate({left: -100 + '%'}, function(){
+          if(n>=viewFilmLiLen-1){
+            n = -1;
+          }
+          viewFilmLi.eq(mvLength-2).prevAll('li').appendTo(viewFilmWrap);
+          viewFilmWrap.css({left:0});
+          viewFilmLi = viewFilmWrap.find('li');
+  
+          indiTitle.animate({left:-inditFirstSize/2+'px'},function(){
+            indiTitleLi.eq(mvLength-2).prevAll('li').appendTo(indiTitle);
+            indiTitle.css({left:0});
+            indiTitleLi = indiTitle.find('li');
+          });
+          indiTitleLi.eq(mvLength+1).addClass('active');
+          btnOk = true;
+        });
+      }
+      indiTitleLi.removeClass('active');
     }
   });
-
   
 
   // title, content text  
-  var BannerLast = viewBannerBgLi.eq(-1*(mvLength+1)).nextAll('li');
-  viewBannerBg.find('ul').prepend(BannerLast);
+
+  var viewBannerH3 = viewBannerBg.find('h3');
+  viewBannerH3.text(MainfilmList[0].title);
 
   viewBannerBgLi.eq(0).show();
   viewBannerBgLi.eq(0).addClass('active');
 
   var ViewBannerTextFn = function(n,k){
+
+    if(n<0){
+      viewBannerH3.text(MainfilmList[7].title);
+    }else{
+      viewBannerH3.text(MainfilmList[n].title);
+    }
+
     if(k!==n){
       viewBannerBgLi.eq(n).css({zIndex:5});
       viewBannerBgLi.eq(k).fadeOut(100,function(){
@@ -136,7 +163,6 @@
   var liLast = viewFilmLi.eq(-1*(mvLength+1)).nextAll('li');
   viewFilmWrap.prepend(liLast);
 
-  
   viewFilmLi = viewFilmWrap.find('li');
   var viewFilmLiLen = viewFilmLi.length;
   console.log(viewFilmLiLen);
@@ -161,12 +187,12 @@
         if(n>=viewFilmLiLen-1){
           n = -1;
         }
-        viewFilmLi.eq(mvLength-1).prevAll('li').appendTo(viewFilmWrap);
+        viewFilmLi.eq(mvLength-2).prevAll('li').appendTo(viewFilmWrap);
         viewFilmWrap.css({left:0});
         viewFilmLi = viewFilmWrap.find('li');
 
-        indiTitle.animate({left:-inditFirstSize+'px'},function(){
-          indiTitleLi.eq(mvLength-1).prevAll('li').appendTo(indiTitle);
+        indiTitle.animate({left:-inditFirstSize/2+'px'},function(){
+          indiTitleLi.eq(mvLength-2).prevAll('li').appendTo(indiTitle);
           indiTitle.css({left:0});
           indiTitleLi = indiTitle.find('li');
         });
@@ -179,16 +205,16 @@
       n-=1;
       ViewBannerTextFn(n,k);
 
-      viewFilmWrap.stop().animate({left: -100 + '%'}, function(){
+      viewFilmWrap.stop().animate({left: 100 + '%'}, function(){
         if(n<0){
           n=viewFilmLiLen-1;
         }
-        viewFilmLi.eq(-1*(mvLength)).nextAll('li').prependTo(viewFilmWrap);
+        viewFilmLi.eq(-1*(mvLength-1)).nextAll('li').prependTo(viewFilmWrap);
         viewFilmWrap.css({left:0});
         viewFilmLi = viewFilmWrap.find('li');
         
-        indiTitle.animate({left:inditFirstSize+'px'},function(){
-          indiTitleLi.eq(-1*(mvLength)).nextAll('li').prependTo(indiTitle);
+        indiTitle.animate({left:inditFirstSize/2+'px'},function(){
+          indiTitleLi.eq(-1*(mvLength-1)).nextAll('li').prependTo(indiTitle);
           indiTitle.css({left:0});
           indiTitleLi = indiTitle.find('li');
         });  
@@ -199,9 +225,6 @@
     indiTitleLi.removeClass('active');
   });
   
-
-
-
 
 
   // end
